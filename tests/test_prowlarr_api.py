3a6891cc94060cf4fa9809586974b0819fd8bc2f
חיים דב טאומן <cdtauman@gmail.com>
2026-09-18 16:56:34 +0300

    test: cover safe Prowlarr redirect handling

--- tests/test_prowlarr_api.py
+++ tests/test_prowlarr_api.py
@@ -31,6 +31,43 @@ class ProwlarrMappingTests(unittest.TestCase):
         finally:
             config.PROWLARR_URL = old_url
 
+    def test_external_redirect_drops_prowlarr_api_key(self):
+        old_url = config.PROWLARR_URL
+        old_key = config.PROWLARR_API_KEY
+        config.PROWLARR_URL = "http://prowlarr:9696"
+        config.PROWLARR_API_KEY = "super-secret"
+        try:
+            redirected = prowlarr_api._redirect_url(
+                "http://prowlarr:9696/download/123",
+                "https://cdn.example.com/file.nzb",
+            )
+            self.assertEqual(redirected, "https://cdn.example.com/file.nzb")
+            self.assertNotIn("X-Api-Key", prowlarr_api._download_headers(redirected))
+            self.assertEqual(
+                prowlarr_api._download_headers(
+                    "http://prowlarr:9696/download/123"
+                )["X-Api-Key"],
+                "super-secret",
+            )
+        finally:
+            config.PROWLARR_URL = old_url
+            config.PROWLARR_API_KEY = old_key
+
+    def test_local_redirect_is_rewritten_to_configured_origin(self):
+        old_url = config.PROWLARR_URL
+        config.PROWLARR_URL = "http://prowlarr:9696"
+        try:
+            redirected = prowlarr_api._redirect_url(
+                "http://prowlarr:9696/download/123",
+                "http://127.0.0.1:9696/download/456",
+            )
+            self.assertEqual(
+                redirected,
+                "http://prowlarr:9696/download/456",
+            )
+        finally:
+            config.PROWLARR_URL = old_url
+
     def test_usenet_release_maps_to_nzb(self):
         mapped = prowlarr_api._map_release({
             "protocol": "usenet",
